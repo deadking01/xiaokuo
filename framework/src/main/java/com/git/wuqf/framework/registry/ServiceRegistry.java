c74b05952355d55f4d88d7a5b6cbbcf4d03d75f1
deadking01 <wuqingfeng234@163.com>
2016-12-15 12:49:09 +0800

    framework init

---
+++ framework/src/main/java/com/git/wuqf/framework/registry/ServiceRegistry.java
@@ -0,0 +1,24 @@
+package com.git.wuqf.framework.registry;
+
+import com.git.wuqf.framework.constant.ZKConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by sdzn-dsj on 2016/12/13.
+ */
+public class ServiceRegistry {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceRegistry.class);
+    private String registryAddress;
+    private ZookeeperUtil zookeeperUtil;
+
+    public ServiceRegistry(String registryAddress) {
+        this.registryAddress = registryAddress;
+        zookeeperUtil=new ZookeeperUtil(registryAddress);
+    }
+
+    public void register(String data) {
+        zookeeperUtil.createNode(ZKConstant.ZK_DATA_PATH,data);
+    }
+}
