7284b0e84ac788357c98f1af9410950013ae2b2a
qingfeng wu <qingfeng wu>
2017-04-19 19:55:31 +0800

    debug test function

--- xiaokuo-rpc/xiaokou-rpc-xiaokuo/src/test/java/com/git/wuqf/rpc/protocol/xiaokuo/XiaokuoProtocolTest.java
+++ xiaokuo-rpc/xiaokou-rpc-xiaokuo/src/test/java/com/git/wuqf/rpc/protocol/xiaokuo/XiaokuoProtocolTest.java
@@ -24,6 +24,9 @@ public class XiaokuoProtocolTest {
         DemoService service = new DemoServiceImpl();
         protocol.export(proxy.getInvoker(service, DemoService.class, URL.valueOf("xiaokuo://127.0.0.1:9020/" + DemoService.class.getName() + "?codec=exchange")));
         service = proxy.getProxy(protocol.refer(DemoService.class, URL.valueOf("xiaokuo://127.0.0.1:9020/" + DemoService.class.getName() + "?codec=exchange")));
-        assertEquals(service.getSize(new String[]{"", "", ""}), 3);
+
+        int size=service.getSize(new String[]{"", "", ""});
+
+        assertEquals(size, 3);
     }
 }
